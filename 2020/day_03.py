73ffd4a554e5917b0cd2b804ee5506b3c28b9795
Scott Macdonald <scott.scotm@gmail.com>
2022-12-05 16:05:10 +0000

    2020: Full Solution for Day 3

--- 2020/day_03.py
+++ 2020/day_03.py
@@ -1,26 +1,28 @@
+from functools import reduce
+
 with open('day_03_input.txt') as myFile:
     data = myFile.read().strip().split("\n")
 
-# data = """..##.......
-# #...#...#..
-# .#....#..#.
-# ..#.#...#.#
-# .#...##..#.
-# ..#.##.....
-# .#.#.#....#
-# .#........#
-# #.##...#...
-# #...##....#
-# .#..#...#.#""".strip().split("\n")
+# Instruction list
+# ---------------
+# Right 1, down 1.
+# Right 3, down 1.
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
 
-right, down = 0, 0
-
-count_trees = 0
-if data[down][right] == '#':
-    count_trees += 1
-while down < len(data) - 1:
-    down += 1
-    right = (right + 3) % len(data[0])
+instructions = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+results = []
+for instruction in instructions:
+    right, down, count_trees = 0, 0, 0
     if data[down][right] == '#':
         count_trees += 1
-print(count_trees)
+    while down < len(data) - 1:
+        down += instruction[1]
+        right = (right + instruction[0]) % len(data[0])
+        if data[down][right] == '#':
+            count_trees += 1
+    results.append(count_trees)
+
+print(f'Answer 1: {results[1]}')
+print(f'Answer 2: {reduce(lambda x, y: x * y, results)}')
