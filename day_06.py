0a2d07ed69adbea2a59c960ae9793166cabf5b42
Scott Macdonald <scott.scotm@gmail.com>
2022-12-06 13:38:35 +0000

    2022: Full Solution for Day 6 - tidied

--- day_06.py
+++ day_06.py
@@ -1,25 +1,22 @@
 def window_gen(this_list, window_size):
-    buffer = []
-    for i in range(0, len(this_list)):
-        if len(buffer) == window_size:
-            buffer.pop(0)
-        buffer.append(this_list[i])
-        if len(buffer) == window_size:
-            yield buffer
+    buffer = list(this_list[:window_size])
+    for i in this_list[window_size:]:
+        buffer.pop(0)
+        buffer.append(i)
+        yield buffer
+
+
+def find_valid_marker(input_stream, window_size):
+    marker_position = window_size
+    for window in window_gen(input_stream, window_size):
+        marker_position += 1
+        if len(set(window)) == window_size:
+            break
+    return marker_position
 
 
 with open('day_06_input.txt') as myFile:
     data = myFile.read().strip()
-
-
-def find_valid_marker(input_stream, window_size=4):
-    input_length = window_size - 1
-    for window in window_gen(input_stream, window_size):
-        input_length += 1
-        if len(set(window)) == len(window):
-            break
-    return input_length
-
 
 # Given test data
 tests = [('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7, 19), ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5, 23),
@@ -27,10 +24,10 @@ tests = [('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7, 19), ('bvwbjplbgvbhsrlpgdmjqwftvn
          ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11, 26)]
 
 for test, expected_1, expected_2 in tests:
-    if find_valid_marker(test) != expected_1:
+    if find_valid_marker(test, 4) != expected_1:
         raise Exception
     if find_valid_marker(test, 14) != expected_2:
         raise Exception
 
-print(f'Answer 1: {find_valid_marker(data)}')
+print(f'Answer 1: {find_valid_marker(data, 4)}')
 print(f'Answer 2: {find_valid_marker(data, 14)}')
